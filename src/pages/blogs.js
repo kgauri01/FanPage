ddfa2d6d8cd29b2fc767d43259a0055651f72bc8
mvekic00 <mvekic00@fesb.hr>
2021-02-19 16:58:19 +0100

    blogs

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,8 +1,10 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
+import BlogsContainer from '../modules/BlogsContainer'
  
 const BlogsPage = () => (
     <HeaderFooterLayout activeTab="Blogs">
+        <BlogsContainer/>
     </HeaderFooterLayout>
 )
  
